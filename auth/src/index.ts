9d3cd8e64007f701b90395b92eb7167e40f2b538
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-04-02 14:45:32 +0100

    added express-async-errors ( a  3rd party library to handle errors correctly in async functions)

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import "express-async-errors"; // 3rd party library to handle errors correctly in async functions
 import { json } from "body-parser";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
