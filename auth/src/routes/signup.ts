2635eb39aed5f9bea2296dbe4764641c79732b48
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-04-02 12:39:39 +0100

    added validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,32 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res, err) => {
-  res.send("Hi there");
-});
+router.post(
+  "/api/users/signup",
+  [
+    // email must be an email
+    body("email").isEmail().withMessage("Email must be valid"),
+    // password must be at least 5 chars long
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    console.log("Creating a user ...");
+
+    res.send({ email, password });
+  }
+);
 
 export { router as signupRouter };
