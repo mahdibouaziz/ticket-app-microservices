70b151bc165d3a34f60d7d8fcb39159fd35260c5
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-04-04 01:39:31 +0100

    added the signin process

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+import { BadRequestError } from "../errors/bad-request-error";
 
 import { validateRequest } from "../middlewares/validate-request";
+import { User } from "../models/user";
+import { Password } from "../services/password";
 
 const router = express.Router();
 
@@ -20,7 +24,34 @@ router.post(
   async (req: Request, res: Response) => {
     const { email, password } = req.body;
 
-    res.send("Hi there");
+    // check if the email already exists
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    // check the password
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordMatch) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    // create jwt and store it as a cookie in the client side
+    const userJwt = await jwt.sign(
+      { id: existingUser.id, email: existingUser.email },
+      process.env.JWT_KEY as string
+    );
+
+    // store jwt on the session object
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
   }
 );
 
