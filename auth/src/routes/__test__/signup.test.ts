786bfeffa96f6f038867c5b662d406249eecf71e
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-04-04 12:21:44 +0100

    added some basic tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,39 @@ it("return a 201 on successful signup", async () => {
     })
     .expect(201);
 });
+
+it("returns a 400 with invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "mahdi.com",
+      password: "mahdi123",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "mahdi@gmail.com",
+      password: "d1",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with missing email and password", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "mahdi@gmail.com",
+    })
+    .expect(400);
+
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      password: "mahdi123",
+    })
+    .expect(400);
+});
