4e1a977d40c20c1cb5ce24f7a4acf10ad7b5e6e5
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-04-04 01:32:21 +0100

    added common request validator middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,27 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res, err) => {
-  res.send("Hi there");
-});
+router.post(
+  "/api/users/signin",
+  [
+    // email must be an email
+    body("email").isEmail().withMessage("Email must be valid"),
+    // password must be at least 5 chars long
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("Password must be not empty"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    res.send("Hi there");
+  }
+);
 
 export { router as signinRouter };
