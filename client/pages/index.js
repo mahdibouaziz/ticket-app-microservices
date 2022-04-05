37be19455cb96844c9c908a4e8e6a46c15b59b17
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-04-05 13:26:42 +0100

    fixed the server rendering VS client rendering inside a NEXT app with k8s

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,29 @@
-export default function Home() {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
   return <div>Landing page</div>;
-}
+};
+
+LandingPage.getInitialProps = async ({ req }) => {
+  // check if we are on the serveror on the browser
+  if (typeof window === "undefined") {
+    // we are on the server
+    // we should name the request to the ingress controller
+    // the base url must be: http://SERVICENAME.NAMESPACE.svc.cluster.local  this will allow us to reach nginx controller
+    const response = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers,
+      }
+    );
+    return response.data;
+  } else {
+    // we are on the browser
+    const response = await axios.get("/api/users/currentuser");
+    return response.data;
+  }
+};
+
+export default LandingPage;
